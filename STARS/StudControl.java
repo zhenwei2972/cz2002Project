1d495e9924089266dc2f5829187778e8edc3970b
Docchopper221 <chienyq221@gmail.com>
2020-11-20 15:28:23 +0800

    Added swap check

--- STARS/StudControl.java
+++ STARS/StudControl.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Mainly handles the functions done by students account
+ * 
+ */
 public class StudControl {
     Filter filter = new Filter();
+    // swaping of courses between 2 students object.
     public void SwapCourse(Student a , Student b , Course m , int modid){
 
         List<Course> result = new ArrayList<Course>();
         List<Course> courselist =  b.getmodules();
         result = filter.byIndex.Invoke(courselist, Integer.toString(modid));
-        
-        b.AddCourse(m);
-        b.RemoveCourse(result.get(0));
-        a.RemoveCourse(m);
-        a.AddCourse(result.get(0));
+        if (result.isEmpty()){
+            System.out.println("Student B does not have " + m.getCourseName() + ":" + modid);
+            return;
+        }
+        else{
+            b.AddCourse(m);
+            b.RemoveCourse(result.get(0));
+            a.RemoveCourse(m);
+            a.AddCourse(result.get(0));
+        }
     }
 }
