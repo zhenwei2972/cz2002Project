4eab1e55bd2a77e900338ff833128cb1c47d42f8
hyunatic <habbo_armageddon@hotmail.com>
2020-11-22 16:23:32 +0800

    Lesson Manager done

--- STARS/CourseManager.java
+++ STARS/CourseManager.java
@@ -5,7 +5,7 @@ public class CourseManager {
     //Functional interface is not other than a abstract method, which we are able polymorph into different methods
     //This method only works on Java 8 onwards 
     @FunctionalInterface
-    interface DataFiltering {
+    interface CourseFiltering {
         public List<Course> Invoke(List<Course> list, String value);
     }
     @FunctionalInterface
@@ -15,11 +15,11 @@ public class CourseManager {
     // index, coursecode, coursename, AU, day, starttime, endtime, classtype, vacancy
     ResultDisplay toDisplayResult = (result) -> result.forEach(x -> System.out.println("Index: " + x.getIndex() + " Course Code: " + x.getCourseCode() + " Vacancy: " + x.getVacancy()));
 
-    DataFiltering byModuleCode = (fullModuleList, value) -> {
+    CourseFiltering byModuleCode = (fullModuleList, value) -> {
         return fullModuleList.stream().filter(x -> value.equals(x.getCourseCode())).collect(Collectors.toList());
     };
 
-    DataFiltering byIndex = (fullModuleList, value) -> {
+    CourseFiltering byIndex = (fullModuleList, value) -> {
         return fullModuleList.stream().filter(x -> Integer.parseInt(value) == x.getIndex()).collect(Collectors.toList());
     };
 
---
+++ STARS/LessonManager.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LessonManager {
+    @FunctionalInterface
+    interface LessonFiltering{
+        public List<Lesson> Invoke(String value);
+    }
+
+    LessonFiltering byIndex = (stringIndex) -> {
+        int value = Integer.parseInt(stringIndex);
+        Database db = new Database();
+        List<Lesson> fullLessonList = db.LessonListDatabase();
+        return fullLessonList.stream().filter(x -> value == x.getIndex()).collect(Collectors.toList());
+    };
+}
