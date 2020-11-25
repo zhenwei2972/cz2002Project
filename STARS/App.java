305b68545024d557a248dfe8ab40c948d146b506
Docchopper221 <chienyq221@gmail.com>
2020-11-25 10:20:10 +0800

    Java doc des

--- STARS/App.java
+++ STARS/App.java
@@ -2,7 +2,21 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 
+/**
+ * This is the root of the program/ the start of program
+ * Launching the login interface.
+ * @author Team Stars
+ * @version 1.0
+ * @since 2020
+ */
 public class App {
+    /**
+     * This is the start of the program
+     * @param args
+     * @throws IOException Exception load error
+     * @throws ParseException Exception casting parsing error
+     * @throws FileNotFoundException Exception of data file not found
+     */
     public static void main(String args[]) throws IOException, ParseException, FileNotFoundException {
         System.out.println("Welcome to STARS Planner!");
         LoginInterface app = new LoginInterface();
--- STARS/Course.java
+++ STARS/Course.java
@@ -1,10 +1,34 @@
+/**
+ * This is the Course Object
+ * it will have a unique index for each course
+ * @author Team Stars
+ * @version 1.0
+ * @since 2020
+ */
 public class Course {
+    /**
+     * interger of index (unique) , vacancy , waitlist and au
+     */
     private int index = 0, vacancy = 0, waitlist =0, au =0;
+    /**
+     * string for the coursecode
+     */
     private String courseCode = "";
 
+    /**
+     * empty constructor use for references 
+     */
     public Course(){
 
     }
+    /**
+     * This is the constructor used to create course with the loaded text file
+     * @param index index of each course
+     * @param courseCode course code for the course
+     * @param vacancy the ammount of slots available for students
+     * @param waitlist the number students queueing for the index course
+     * @param au the number of au is for the module
+     */
     public Course(int index, String courseCode, int vacancy, int waitlist, int au){
         this.index = index;
         this.courseCode = courseCode;
@@ -12,33 +36,75 @@ public class Course {
         this.waitlist = waitlist;
         this.au = au;
     }
+    //----------------------Getter----------------------//
+    /**
+     * getter method for index
+     * @return integer course index
+     */
     public int getIndex(){
         return index;
     }
+    /**
+     * getter method for course code
+     * @return string course code
+     */
     public String getCourseCode(){
         return courseCode;
     }
+    /**
+     * getter for slots available
+     * @return integer course vacancy
+     */
     public int getVacancy(){
         return vacancy;
     }
+    /**
+     * getter for numbers in queue
+     * @return integer waitlist
+     */
     public int getWaitlist(){
         return waitlist;
     }
+    /**
+     * getter mehtod for AU
+     * @return integer of course AU
+     */
     public int getAU(){
         return au;
     }
+    //----------------------setter--------------------//
+    /**
+     * setter method for index
+     * @param newIndex is the new index to update
+     */
     public void setIndex(int newIndex){
         this.index = newIndex;
     }
+    /**
+     * setter method for course code
+     * @param newcourseCode is the new course code to update
+     */
     public void setCourseCode(String newcourseCode){
         this.courseCode = newcourseCode;
     }
+    /**
+     * setter method for course available slots
+     * @param newVacancy is the new vacancy to update
+     */
     public void setVacancy(int newVacancy){
         this.vacancy = newVacancy;
     }
+    /**
+     * setter method for course waitlist
+     * @param newWaitList is the new course waitlist to update
+     */
     public void setWaitlist(int newWaitlist){
         this.waitlist = newWaitlist;
     }
+    /**
+     * setter method for course au
+     * @param newau is the new course au to update
+     */
     public void setAu(int newAU){
         this.au = newAU;
     }
