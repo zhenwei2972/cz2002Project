1998390211b68296eeb468c29e2a688ff8459c98
Docchopper221 <chienyq221@gmail.com>
2020-11-25 10:41:08 +0800

    Database java doc done

--- STARS/Database.java
+++ STARS/Database.java
@@ -7,8 +7,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 
+/**
+ * This is the database class,
+ * handles loading of data from text and export to text
+ * @author Team Stars
+ * @version 1.0
+ * @since 2020
+ */
 public class Database {
-
+    /**
+     * This function reads the lesson.txt and create all the lesson object
+     * @return the list of lesson object
+     */
     public List<Lesson> LessonListDatabase() {
         List<Lesson> fulllessonlist = new ArrayList<Lesson>();
         ArrayList<String> list = new ArrayList<String>();
@@ -32,7 +42,11 @@ public class Database {
         }
         return fulllessonlist;
     }
-
+    /**
+     * write out the lessons to a lesson.txt file 
+     * @param fullLessonList is the list of lesson object
+     * @throws IOException is to handle write out exception or fails
+     */
     public void UpdateLessonDatabase(List<Lesson> fullLessonList) throws IOException {
         FileWriter myWriter = new FileWriter("Database\\lesson.txt");
         // Index, Coursecode, Coursename, AU, Day, starttime, endtime, classtype
@@ -47,7 +61,11 @@ public class Database {
         });
         myWriter.close();
     }
-
+    /**
+     * this function loads the admin.txt
+     * create a list of admin object(account)
+     * @return a list of admin object
+     */
     public List<Admin> AdminUserDatabase() {
         List<Admin> fullAdminList = new ArrayList<Admin>();
         ArrayList<String> list = new ArrayList<String>();
@@ -68,7 +86,11 @@ public class Database {
         }
         return fullAdminList;
     }
-
+    /**
+     * this function write out the admin.txt
+     * @param fullAdminList the list of admin object
+     * @throws IOException handles the fail and writeout error
+     */
     public void UpdateAdminDatabase(List<Admin> fullAdminList) throws IOException {
         FileWriter myWriter = new FileWriter("Database\\admin.txt");
         // username, password
@@ -82,7 +104,11 @@ public class Database {
         });
         myWriter.close();
     }
-
+    /**
+     * Load and read student.txt 
+     * create the list of student object(account)
+     * @return the list of student object
+     */
     public List<Student> StudentDatabase() {
         List<Student> fullStudentList = new ArrayList<Student>();
         ArrayList<String> list = new ArrayList<String>();
@@ -103,7 +129,11 @@ public class Database {
         }
         return fullStudentList;
     }
-
+    /**
+     * write out the list of student data to txt
+     * @param fullStudentList the lsit of student acount    
+     * @throws IOException handles the error and fails of write out
+     */
     public void UpdateStudentDatabase(List<Student> fullStudentList) throws IOException {
         FileWriter myWriter = new FileWriter("Database\\student.txt");
         // MatricNo, Username, Password, PartTimeStudent (true / false)
@@ -119,6 +149,11 @@ public class Database {
         myWriter.close();
     }
 
+    /**
+     * Load and read course.txt 
+     * create the list of course object(account)
+     * @return the list of course object
+     */
     public List<Course> CourseListDatabase() {
         List<Course> fullcourselist = new ArrayList<Course>();
         ArrayList<String> list = new ArrayList<String>();
@@ -140,6 +175,11 @@ public class Database {
         return fullcourselist;
     }
 
+    /**
+     * this function write out the course list to course.txt
+     * @param fullCourseList the course list
+     * @throws IOException handle the error and write out fails
+     */
     public void UpdateCourseDatabase(List<Course> fullCourseList) throws IOException {
         FileWriter myWriter = new FileWriter("Database\\course.txt");
         // index, coursecode, vacancy, waitlist
@@ -154,7 +194,11 @@ public class Database {
         });
         myWriter.close();
     }
-
+    /**
+     * Load and read accessperiod.txt 
+     * create the list of acessperiod object(account)
+     * @return the list of accessperiod object
+     */
     public List<AccessPeriod> AccessPeriodDatabase() {
         List<AccessPeriod> fullAccessPeriodlist = new ArrayList<AccessPeriod>();
         ArrayList<String> list = new ArrayList<String>();
@@ -176,7 +220,11 @@ public class Database {
         }
         return fullAccessPeriodlist;
     }
-
+     /**
+     * this function write out the accessperiod to accessperiod.txt
+     * @param fullAccessPeriodlist the accessperiod
+     * @throws IOException handle the error and write out fails
+     */
     public void UpdateAccessListDatabase(List<AccessPeriod> fullAccessPeriodlist) throws IOException {
         FileWriter myWriter = new FileWriter("Database\\accessperiod.txt");
         // index, coursecode, vacancy, waitlist
