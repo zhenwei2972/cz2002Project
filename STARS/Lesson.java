b76d10527f2dd0b4ddd82815dda1b0cbf41539c4
Docchopper221 <chienyq221@gmail.com>
2020-11-25 13:16:11 +0800

    Lesson and Lesson Manager done

--- STARS/Lesson.java
+++ STARS/Lesson.java
@@ -1,11 +1,31 @@
+/**
+ * This is a Lesson Object class
+ * it stores the lessons:
+ * index , coursecode, day , time ,au and lesson type eg. Lab/Tut/Lec
+ * @author Team Stars
+ * @version 1.0
+ * @since 2020
+ */
 public class Lesson {
-    // Time Format 20:15:40 hh:mm:ss
-    // data column
-    // index,coursecode,coursename,AU,day,starttime,endtime,classtype
+    /** Time Format 20:15:40 hh:mm:ss
+    * data column
+    * index,coursecode,coursename,AU,day,starttime,endtime,classtype
+    */
     private int index = 0, au = 0;
     private String coursecode = "", coursename = "", day = "", classtype = "", venue ="";
     private int starttime = 0, endtime = 0;
-    
+    /**
+     * This is the lesson constructor
+     * @param index as the course index for refernce
+     * @param coursecode as the course code
+     * @param coursename as the course name
+     * @param au as the au for the course
+     * @param day as which day the lesson is on
+     * @param starttime as the starting time of the lesson  
+     * @param endtime as the ending time of the lesson
+     * @param classtype as the type of lesson it is Lab/Lec/Tut
+     * @param venue as the place the lesson is held
+     */
     public Lesson(int index, String coursecode, String coursename, int au, String day, int starttime, int endtime,String classtype,String venue){
         this.index = index;
         this.coursecode = coursecode;
@@ -18,124 +38,142 @@ public class Lesson {
     }
     
     /** 
-     * @return int
+     * getter method for index
+     * @return integer index
      */
     public int getIndex(){
         return this.index;
     }
     
     /** 
-     * @return String
+     * getter method for course code
+     * @return String course code
      */
     public String getCourseCode(){
         return this.coursecode;
     }
     
-    /** 
-     * @return String
+    /**
+     * getter method to get course name
+     * @return String course name
      */
     public String getCourseName(){
         return this.coursename;
     }
     
-    /** 
-     * @return int
+    /**
+     * getter method for AU
+     * @return integer course AU
      */
     public int getAU(){
         return this.au;
     }
     
     /** 
-     * @return String
+     * getter method for Day
+     * @return String Day
      */
     public String getDay(){
         return this.day;
     }
     
     /** 
-     * @return int
+     * getter method for start time
+     * @return integer start time
      */
     public int getStartTime(){
         return this.starttime;
     }
     
     /** 
-     * @return int
+     * getter method for end time
+     * @return integer end time
      */
     public int getEndTime(){
         return this.endtime;
     }
     
     /** 
-     * @return String
+     * getter method for class type
+     * @return String class type 
      */
     public String getClassType(){
         return this.classtype;
     }
-     /** 
-     * @return String
-     */
+     /**
+      * getter method for venue
+      * @return String venue
+      */
     public String getVenue(){
         return this.venue;
     }
     
-    /** 
-     * @param newIndex
+    /**
+     * setter method for index
+     * @param newIndex as the new index
      */
     public void setIndex(int newIndex){
         this.index = newIndex;
     }
     
     /** 
-     * @param newCourseCode
+     * setter method for course code
+     * @param newCourseCode as the new course code
      */
     public void setCourseCode(String newCourseCode){
         this.coursecode = newCourseCode;
     }
     
     /** 
-     * @param newCouseName
+     * setter method for course name
+     * @param newCouseName as the new course name
      */
     public void setCourseName(String newCouseName){
         this.coursename = newCouseName;
     }
     
     /** 
-     * @param newAU
+     * setter method for AU
+     * @param newAU as the new AU
      */
     public void setAU(int newAU){
         this.au = newAU;
     }
     
     /** 
-     * @param newDay
+     * setter method for Day
+     * @param newDay as the new day value
      */
     public void setDay(String newDay){
         this.day = newDay;
     }
     
     /** 
-     * @param newStartTime
+     * setter method for start time
+     * @param newStartTime as the new start time 
      */
     public void setStartTime(int newStartTime){
         this.starttime = newStartTime;
     }
     
     /** 
-     * @param newEndTime
+     * setter method for end time
+     * @param newEndTime as the new end time
      */
     public void setEndTime(int newEndTime){
         this.endtime = newEndTime;
     }
     
-    /** 
-     * @param newClassType
+    /**
+     * setter method for class type 
+     * @param newClassType as the new class type
      */
     public void setClassType(String newClassType){
         this.classtype = newClassType;
     }
     /** 
-     * @param venue
+     * stter method for the venue
+     * @param venue as the new venue
      */
     public void setVenue(String newVenue){
         this.venue = newVenue;
--- STARS/LessonManager.java
+++ STARS/LessonManager.java
@@ -3,24 +3,53 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.Comparator;
 
+/**
+ * This is the lesson manager class.
+ * it handles the filtering for lesson list and timetables
+ * @author Team Stars
+ * @version 1.0
+ * @since 2020
+ */
 public class LessonManager {
+     /**
+     * a Functional interface is not other than a abstract method, which we are able polymorph into different methods
+     * This method only works on Java 8 onwards
+     * interface for flitering
+     */
     @FunctionalInterface
     interface LessonFiltering{
         public List<Lesson> Invoke(String value);
     }
-
+     /**
+     * a Functional interface is not other than a abstract method, which we are able polymorph into different methods
+     * This method only works on Java 8 onwards
+     * interface for printing timetable
+     */
     @FunctionalInterface
     interface TimeTableFiltering{
         public List<Lesson> Invoke(List<Integer> indexlist);
     }
+    /**
+     * using the database class
+     */
     Database db = new Database();
+    /**
+     * using the database class to take all the lesson list
+     */
     List<Lesson> fullLessonList = db.LessonListDatabase();
- 
+    
+    /**
+     * filter by index
+     * @param stringIndex as the course index to filter
+      */
     LessonFiltering byIndex = (stringIndex) -> {
         int value = Integer.parseInt(stringIndex);
         return fullLessonList.stream().filter(x -> value == x.getIndex()).collect(Collectors.toList());
     };
-
+    /**
+     * to get lessons for Monday only
+     * @param indexList is the list of indexs to filter
+     */
     TimeTableFiltering getMondayClasses = (indexList) ->{
         List<Lesson> result = new ArrayList<Lesson>();
         for(Integer i: indexList){
@@ -30,7 +59,10 @@ public class LessonManager {
         List<Lesson> sortedTime = result.stream().sorted(Comparator.comparingInt(Lesson::getStartTime)).collect(Collectors.toList());
         return sortedTime;
     };
-
+    /**
+     * to get lessons for Tuesday only
+     * @param indexList is the list of indexs to filter
+     */
     TimeTableFiltering getTuesdayClasses = (indexList) ->{
         List<Lesson> result = new ArrayList<Lesson>();
         for(Integer i: indexList){
@@ -40,7 +72,10 @@ public class LessonManager {
         List<Lesson> sortedTime = result.stream().sorted(Comparator.comparingInt(Lesson::getStartTime)).collect(Collectors.toList());
         return sortedTime;
     };
-
+    /**
+     * to get lessons for Wednessday only
+     * @param indexList is the list of indexs to filter
+     */
     TimeTableFiltering getWednesdayClasses = (indexList) ->{
         List<Lesson> result = new ArrayList<Lesson>();
         for(Integer i: indexList){
@@ -50,7 +85,10 @@ public class LessonManager {
         List<Lesson> sortedTime = result.stream().sorted(Comparator.comparingInt(Lesson::getStartTime)).collect(Collectors.toList());
         return sortedTime;
     };
-
+    /**
+     * to get lessons for Thursday only
+     * @param indexList is the list of indexs to filter
+     */
     TimeTableFiltering getThursdayClasses = (indexList) ->{
         List<Lesson> result = new ArrayList<Lesson>();
         for(Integer i: indexList){
@@ -60,7 +98,10 @@ public class LessonManager {
         List<Lesson> sortedTime = result.stream().sorted(Comparator.comparingInt(Lesson::getStartTime)).collect(Collectors.toList());
         return sortedTime;
     };
-    
+    /**
+     * to get lessons for Friday only
+     * @param indexList is the list of indexs to filter
+     */ 
     TimeTableFiltering getFridayClasses = (indexList) ->{
         List<Lesson> result = new ArrayList<Lesson>();
         for(Integer i: indexList){
